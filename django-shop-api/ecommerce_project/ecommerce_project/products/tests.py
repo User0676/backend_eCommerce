f83d84dba5f44a0b8741d6679c9a91300a115391
alizhumataeyev <japangod202203@gmail.com>
2024-12-13 17:42:20 +0500

    test

--- django-shop-api/ecommerce_project/ecommerce_project/products/tests.py
+++ django-shop-api/ecommerce_project/ecommerce_project/products/tests.py
@@ -23,6 +23,6 @@ class ProductModelTest(TestCase):
         self.assertEqual(self.category.name, "Electronics")
 
     def test_get_product_detail(self):
-        response = self.client.get(f"/api/products/{self.product.id}/")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = self.client.get(f'/api/products/{self.product.id}/')
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data["name"], "Test Product")
